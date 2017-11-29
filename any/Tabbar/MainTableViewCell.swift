afd32d1d9a87e29fa6ce6a846b2da2f35bcf03a6
yjs_diveroid <yjs@diveroid.com>
2017-11-29 16:20:09 +0900

    init commit

---
+++ any/Tabbar/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  any
+//
+//  Created by 윤종서 on 2017. 11. 25..
+//  Copyright © 2017년 jongseo. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ any/Tabbar/MainViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewCell.swift
+//  any
+//
+//  Created by 윤종서 on 2017. 11. 25..
+//  Copyright © 2017년 jongseo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewCell: UICollectionViewCell {
+    
+}
